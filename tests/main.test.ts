3a7e6518d5257508a57977f22387c1ba57fae046
OgabekYuldoshev <yuldashoff1@gmail.com>
2024-11-07 00:21:37 +0500

    feat(progress): createBumpy

--- tests/main.test.ts
+++ tests/main.test.ts
@@ -1,7 +1,20 @@
 import { describe, expect, test } from "vitest";
+import { createBumpy } from "../src/createBumpy";
+import { z } from "zod";
 
 describe("Dumpy Database tests", () => {
-  test("Test", () => {
-    expect(true).toBe(true)
+  test("Test", async () => {
+    const db = await createBumpy({
+      path: './meta/db.json',
+      schema: {
+        utils: z.object({
+          test: z.string()
+        })
+      }
+    })
+
+
+    expect(db.name).toBe("db")
+    expect(db.utils()).toBe("Salom")
   })
 })
